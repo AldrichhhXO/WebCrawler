106b3630454271336607fac889caaf5efd8fd260
AldrichhhXO <Dridgereb@yahoo.com>
2019-05-02 20:24:14 -0700

    PyMySql implementation in Memory.py

--- Memory.py
+++ Memory.py
@@ -1,9 +1,74 @@
 import os
 
-"""
-	This file will be responsible for keeping track of the crawled 
+# Importing the MySQL module
+import pymysql
 
 """
+	This file will be responsible for keeping track of the crawled links within the website.
+	Will have two different types of memory; depending on how you want to keep track of the  info
+		1. Text file; which will be updated through every iteration of the crawl.
+		2. Database: Which will be used the same way but in a more organized manner.
+
+
+	ToDo (If time): Convert the sql functons below into a sql file and read from there.
+
+"""
+
+# Connection to the  database
+con = pymysql.connect('localhost', 'root', 'ASZNkevin1')
+
+with con:
+	cur = con.cursor()
+	print(cur)
+
+# This wil create the database instance in the MySQL server.
+def create_database():
+	print("### Creating database ###" + "\n")
+	con = pymysql.connect('localhost', 'root', 'ASZNkevin1')
+	with con:
+		cur = con.cursor()
+		cur.execute("CREATE DATABASE IF NOT EXISTS WebCrawler;")
+		cur.close()
+	initialize_tables()
+		
+
+# Initializing the tables for the webcrawler
+def initialize_tables():
+	print("### Initializing Tables ###" + "\n")
+	con = pymysql.connect('localhost', 'root', 'ASZNkevin1')
+	with con:
+		cur = con.cursor()
+		
+		cur.execute("CREATE TABLE IF NOT EXISTS Queue \
+			(UrlID INT AUTO_INCREMENT \
+			Url CHAR(100) NOT NULL, \
+			 Crawled VARCHAR(5) NOT NULL DEFAULT 'FALSE', \
+			 PRIMARY KEY(UrlID));")
+
+		#  Todo: Figure out what to store in this table. ############
+		"""
+			StockData table:
+				StockName: Name of the stock that we scrape.
+				urlID: Reference as to where the url came from. (may not need)
+
+		"""
+		cur.execute("CREATE TABLE IF NOT EXISTS StockData \
+			(StockName VARCHAR(20), \
+			urlID INT NOT NULL \
+			FOREIGN KEY (urlID) REFERENCES Queue(UrlID));")
+
+
+# Inserts the link into the database.
+def insert_link_to_db(link):
+	con = pymysql.connect('localhost', 'root', 'ASZNkevin1')
+	with con:
+		cur = con.cursor()
+
+		cur.execute("INSERT INTO Queue (Url) VALUES (link)")
+		cur.close()
+
+
+
 
 # Each website you crawl is a separate project (folder)
 def create_project_dir(directory):
